8340ede0afd85fc45eadd457b502be6b1defdd03
irvinsrz <144683707+irvinsrz@users.noreply.github.com>
2026-07-23 22:42:53 +0800

    Add files via upload

--- lesson-11/11.todo-list.js
+++ lesson-11/11.todo-list.js
@@ -1,9 +1,59 @@
-const todoList = [];
+const todoList = [{
+  name:'make dinner', 
+  dueDate: '2020-12-2'}, 
+  {
+  name:'wash dishes',
+  dueDate: '2021-10-3'}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0;i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+
+    //this is shortcut for this ^^^
+    const {name, dueDate} = todoObject;
+
+    const html = `
+      <p>
+        ${name} ${dueDate}
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        ">Delete</button> 
+      </p>
+    `;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
-
   const name = inputElement.value;
-  todoList.push(name);
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate
+  });
   console.log(todoList);
+
+  
+  inputElement.value = '';
+
+  renderTodoList();
 }
